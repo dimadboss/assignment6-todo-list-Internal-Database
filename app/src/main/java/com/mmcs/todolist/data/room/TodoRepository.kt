31e064f545286ff9cd5235f63263145d526d1842
dmitry_drozdov <dmitry.drozdov@slcode.com>
2022-05-07 19:36:03 +0300

    add todo repo

---
+++ app/src/main/java/com/mmcs/todolist/data/room/TodoRepository.kt
@@ -0,0 +1,15 @@
+package com.mmcs.todolist.data.room
+
+import com.mmcs.todolist.data.room.dao.TodoDao
+import com.mmcs.todolist.data.room.entities.TodoEntity
+
+class TodoRepository(private val dictionaryDao: TodoDao) {
+
+    suspend fun selectAllTodos(): List<TodoEntity> = dictionaryDao.selectAllItems()
+
+    suspend fun insertTodo(todo: TodoEntity) = dictionaryDao.insertItem(todo)
+
+    suspend fun deleteTodo(todo: TodoEntity) = dictionaryDao.deleteItem(todo)
+
+    suspend fun updateTodo(todo: TodoEntity) = dictionaryDao.updateItem(todo)
+}
